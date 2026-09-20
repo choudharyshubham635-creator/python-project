1d526e144b4d789655100a82119f84da3a2444e8
choudharyshubham635-creator <choudharyshubham635@gmail.com>
2026-09-20 11:43:39 +0530

    Fix incorrect method casing in Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,3 @@
 import streamlit as str
 
-str.Write('Hello, I am shubham welcome to my quiz zone.. hope you like the game. you my please proceed further for gaming...')
+str.write('Hello, I am shubham welcome to my quiz zone.. hope you like the game. you my please proceed further for gaming...')
