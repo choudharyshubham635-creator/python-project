6b418be2e6a6d3d61aa972145eef1812d4b62ab8
choudharyshubham635-creator <choudharyshubham635@gmail.com>
2026-09-20 11:51:43 +0530

    Add age input and eligibility check for quiz

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,10 @@
 import streamlit as str
 
 str.write('Hello, I am shubham welcome to my quiz zone.. hope you like the game. you my please proceed further for gaming...')
+age = str.number_input('Enter ur Age....')
+if age>=18:
+  str.write("You are eligible for licence...")
+  str.balloons()
+  str.snow()
+else:
+  str.write('You are not eligible...')
